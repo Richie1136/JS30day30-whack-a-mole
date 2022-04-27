839e1914ce18919fe587fc08a47582751a4a70da
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-04-27 13:13:40 -0400

    Getting a random hole using math.random and getting the index

--- main.js
+++ main.js
@@ -6,3 +6,10 @@ const moles = document.querySelectorAll('.mole')
 const randTime = (min, max) => {
   return Math.round(Math.random() * (max - min) + min)
 }
+
+const randHole = (holes) => {
+  console.log(holes.length)
+  const index = Math.floor(Math.random() * holes.length)
+  const hole = holes[index]
+  console.log(hole)
+}
