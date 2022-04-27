abaddee81f3de83d90df9cd838e416fdeaf0b0ac
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-04-27 13:16:51 -0400

    Created a lastHole variable which will store the hole variable and if the lastHole is the same as the hole then im going to rerun the randHole function

--- main.js
+++ main.js
@@ -2,14 +2,20 @@ const holes = document.querySelectorAll('.hole')
 const scoreBoard = document.querySelector('.score')
 const moles = document.querySelectorAll('.mole')
 
+let lastHole;
+
 
 const randTime = (min, max) => {
   return Math.round(Math.random() * (max - min) + min)
 }
 
 const randHole = (holes) => {
-  console.log(holes.length)
   const index = Math.floor(Math.random() * holes.length)
   const hole = holes[index]
   console.log(hole)
+  if (hole === lastHole) {
+    console.log("Thats the same one")
+    randHole(hole)
+  }
+  lastHole = hole
 }
